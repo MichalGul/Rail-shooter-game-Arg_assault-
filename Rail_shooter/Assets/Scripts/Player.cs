c690574d3511e6713d352e69449dc30ca8e82fb7
DESKTOP-NGBNHNQ\Michał <mwarioxin@gmail.com>
2018-09-06 22:11:28 +0200

    Begin to build cross platform controlls

---
+++ Rail_shooter/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class Player : MonoBehaviour {
+
+    [Tooltip ("In ms^-1")][SerializeField]
+    float Speed = 4f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float verticalThor = CrossPlatformInputManager.GetAxis("Vertical");
+        //make this frame independent
+        float xOffset = xThrow * Speed * Time.deltaTime;
+
+        print(xOffset);
+	}
+}
